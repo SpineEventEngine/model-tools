3e2b18eb9c4d52ea602630f105e2a9c0999f2a16
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2019-02-20 17:27:44 +0200

    Apply compile-time validation to `lifecycle` types

---
+++ model/model-verifier/src/main/java/io/spine/model/verify/MessageTypeValidator.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.model.verify;
+
+import io.spine.code.proto.MessageType;
+import io.spine.server.model.InsufficientMessageTypeError;
+
+import java.util.function.Predicate;
+
+final class MessageTypeValidator {
+
+    private final Predicate<MessageType> predicate;
+    private final String errorMessage;
+
+    MessageTypeValidator(Predicate<MessageType> predicate, String errorMessage) {
+        this.predicate = predicate;
+        this.errorMessage = errorMessage;
+    }
+
+    void check(MessageType type) {
+        if (!predicate.test(type)) {
+            throwNonMatchError(type);
+        }
+    }
+
+    private void throwNonMatchError(MessageType type) {
+        throw new InsufficientMessageTypeError(errorMessage, type);
+    }
+}
